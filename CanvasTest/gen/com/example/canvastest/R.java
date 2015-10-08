7642f343b910846722a113b987b41fc2ae1822f6
Zhenghaotao <zhenghaotao@foxmail.com>
2015-10-08 12:43:47 +0800

    canvas学习

--- CanvasTest/gen/com/example/canvastest/R.java
+++ CanvasTest/gen/com/example/canvastest/R.java
@@ -35,7 +35,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int circle=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int oval=0x7f050007;
+        public static final int pentagon=0x7f050009;
+        public static final int rect=0x7f050005;
+        public static final int round_rect=0x7f050006;
+        public static final int square=0x7f050004;
+        public static final int triangle=0x7f050008;
     }
     public static final class style {
         /** 
--- CanvasTest/src/com/example/canvastest/MainActivity.java
+++ CanvasTest/src/com/example/canvastest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.canvastest;
 
+import com.example.canvastest.view.MyView;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -9,15 +11,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new MyView(this));
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
     
 }
